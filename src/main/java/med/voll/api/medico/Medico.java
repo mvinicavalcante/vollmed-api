44b4fbb33ab3a9b3a20000a4b0e4aa14ce8ef5ae
Marcos Vinícius Cavalcante <mvinic2011@gmail.com>
2023-10-23 00:26:29 -0300

    Parte 1

--- src/main/java/med/voll/api/medico/Medico.java
+++ src/main/java/med/voll/api/medico/Medico.java
@@ -26,12 +26,34 @@ public class Medico {
     //Medico mesmo
     private Endereco endereco;
 
+    private Boolean ativo;
+
     public Medico(DadosCadastroMedico dados) {
+        this.ativo = true;
         this.nome = dados.nome();
         this.email = dados.email();
         this.telefone = dados.telefone();
         this.crm = dados.crm();
-        this.endereco = new Endereco(dados.endereco());
         this.especialidade = dados.especialidade();
+        this.endereco = new Endereco(dados.endereco());
+    }
+
+
+    public void atualizarInformacoes(DadosAtualizacaoMedico dados) {
+        if(dados.nome() != null) {
+            this.nome = dados.nome();
+        }
+        if(dados.telefone() != null) {
+            this.telefone = dados.telefone();
+        }
+        if(dados.endereco() != null) {
+            this.endereco.atualizarInformacoes(dados.endereco());
+        }
+
+
+    }
+
+    public void excluir(Long id) {
+        this.ativo = false;
     }
 }
